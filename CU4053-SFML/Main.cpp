6e44eeaacd052a76fd2e78ac155cb3ac302c2dff
utkishu <152878833+utkishu@users.noreply.github.com>
2025-02-17 13:25:52 +0000

    Initial Commit

---
+++ CU4053-SFML/Main.cpp
@@ -0,0 +1,87 @@
+﻿// Main
+// Entry point of the application.
+// Handles window creation and window events.
+// Contains the game loop, updates input class and contains the level objects.
+
+#include <iostream>
+#include "Level.h"
+
+void windowProcess(sf::RenderWindow* window, Input* in)
+{
+	// Handle window events.
+	sf::Event event;
+	while (window->pollEvent(event))
+	{
+		switch (event.type)
+		{
+		case sf::Event::Closed:
+			window->close();
+			break;
+		case sf::Event::Resized:
+			window->setView(sf::View(sf::FloatRect(0.f, 0.f, (float)event.size.width, (float)event.size.height)));
+			break;
+		case sf::Event::KeyPressed:
+			// update input class
+			in->setKeyDown(event.key.code);
+			break;
+		case sf::Event::KeyReleased:
+			//update input class
+			in->setKeyUp(event.key.code);
+			break;
+		case sf::Event::MouseMoved:
+			//update input class
+			in->setMousePosition(event.mouseMove.x, event.mouseMove.y);
+			break;
+		case sf::Event::MouseButtonPressed:
+			if (event.mouseButton.button == sf::Mouse::Left)
+			{
+				//update input class
+				in->setMouseLDown(true);
+			}
+			else if (event.mouseButton.button == sf::Mouse::Right)
+			{
+				in->setMouseRDown(true);
+			}
+			break;
+		case sf::Event::MouseButtonReleased:
+			if (event.mouseButton.button == sf::Mouse::Left)
+			{
+				//update input class
+				in->setMouseLDown(false);
+			}
+			else if (event.mouseButton.button == sf::Mouse::Right)
+			{
+				in->setMouseRDown(false);
+			}
+			break;
+		default:
+			// don't handle other events
+			break;
+		}
+	}
+}
+
+int main()
+{
+	//Create the window
+	sf::RenderWindow window(sf::VideoMode(1200, 675), "CMP105_W2");
+
+	// Initialise input and level objects.
+	Input input;
+	Level level(&window, &input);
+
+	// Game Loop
+	while (window.isOpen())
+	{
+		//Process window events
+		windowProcess(&window, &input);
+
+		// Call standard game loop functions (input, update and render)
+		level.handleInput();
+		level.update();
+		level.render();
+
+	}
+
+}
+
